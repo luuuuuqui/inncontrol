46c3cc7161308481039ff41882a15e0480ed1446
Lucas Duarte <luscadrt@gmail.com>
2025-12-10 21:21:26 +0000

    criação e teste de "index.py".

---
+++ src/index.py
@@ -0,0 +1,10 @@
+import streamlit as st
+
+class IndexUI:
+    @staticmethod
+    def main():
+        st.title("Hello, Streamlit!")
+
+if __name__ == "__main__":
+    IndexUI.main()
+else: exit('Esse arquivo deve ser executado diretamente pelo Streamlit.')
