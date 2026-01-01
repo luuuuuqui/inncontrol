6af516b0aa092d476ce1311a9383f268a3b4bdf9
Lucas Duarte <196474158+luuuuuqui@users.noreply.github.com>
2026-01-01 18:50:40 -0300

    mudança na classe hóspede

--- src/models/hospede.py
+++ src/models/hospede.py
@@ -1,9 +1,14 @@
 class Hospede:
-    def __init__(self, id_usuario: int, endereco: str) -> None:
+    def __init__(self, id_hospede: int, id_usuario: int, endereco: str) -> None:
+        self.set_id_hospede(id_hospede)
         self.set_id_usuario(id_usuario)
         self.set_endereco(endereco)
     
     # Setters:
+    def set_id_hospede(self, id_hospede: int) -> None: 
+        if id_hospede <= 0: raise ValueError("ID do hóspede deve ser um inteiro positivo.")
+        self._id_hospede = id_hospede
+
     def set_id_usuario(self, id_usuario: int) -> None: 
         if id_usuario <= 0: raise ValueError("ID do usuário deve ser um inteiro positivo.")
         self._id_usuario = id_usuario
@@ -13,6 +18,8 @@ class Hospede:
         self._endereco = endereco
 
     # Getters:
+    def get_id_hospede(self) -> int: return self._id_hospede
+
     def get_id_usuario(self) -> int: return self._id_usuario
 
     def get_endereco(self) -> str: return self._endereco
@@ -20,6 +27,7 @@ class Hospede:
     # Métodos:
     def to_dict(self) -> dict:
         return {
+            "id_hospede": self.get_id_hospede(),
             "id_usuario": self.get_id_usuario(),
             "endereco": self.get_endereco()
         }
