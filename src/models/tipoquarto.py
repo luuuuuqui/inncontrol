e6c1de4bbf3b3eb3d9ee176167594e6b460f6afd
Lucas Duarte <196474158+luuuuuqui@users.noreply.github.com>
2026-01-06 12:17:01 -0300

    refactor: encapsulação dos atributos de tipoquarto

--- src/models/tipoquarto.py
+++ src/models/tipoquarto.py
@@ -20,45 +20,45 @@ class TipoQuarto:
     def set_id_tipo(self, id_tipo: int) -> None:
         if id_tipo <= 0:
             raise ValueError("ID do tipo de quarto deve ser um inteiro positivo.")
-        self._id_tipo = id_tipo
+        self.__id_tipo = id_tipo
 
     def set_nome(self, nome: str) -> None:
         if nome == "":
             raise ValueError("Nome do tipo de quarto não pode ser vazio.")
-        self._nome = nome
+        self.__nome = nome
 
     def set_descricao(self, descricao: str) -> None:
         if descricao == "":
             raise ValueError("Descrição do tipo de quarto não pode ser vazia.")
-        self._descricao = descricao
+        self.__descricao = descricao
 
     def set_capacidade(self, capacidade: int) -> None:
         if capacidade <= 0:
             raise ValueError(
                 "Capacidade do tipo de quarto deve ser um inteiro positivo."
             )
-        self._capacidade = capacidade
+        self.__capacidade = capacidade
 
     def set_valor_diaria(self, valor_diaria: decimal) -> None:
         if valor_diaria < 0:
             raise ValueError("Valor da diária não pode ser negativo.")
-        self._valor_diaria = valor_diaria
+        self.__valor_diaria = valor_diaria
 
     # Getters:
     def get_id_tipo(self) -> int:
-        return self._id_tipo
+        return self.__id_tipo
 
     def get_nome(self) -> str:
-        return self._nome
+        return self.__nome
 
     def get_descricao(self) -> str:
-        return self._descricao
+        return self.__descricao
 
     def get_capacidade(self) -> int:
-        return self._capacidade
+        return self.__capacidade
 
     def get_valor_diaria(self) -> decimal:
-        return self._valor_diaria
+        return self.__valor_diaria
 
     # Métodos:
     def to_dict(self) -> dict:
