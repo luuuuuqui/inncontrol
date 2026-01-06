580b5f6f8bd7de2e435856ae1997e04012da55f1
Lucas Duarte <196474158+luuuuuqui@users.noreply.github.com>
2026-01-06 17:14:41 -0300

    feat(views): adiciona tipo quarto ao views.py

--- src/views.py
+++ src/views.py
@@ -1,20 +1,26 @@
+from decimal import Decimal
+
 from dao.usuariodao import UsuarioDAO
+from dao.hospededao import HospedeDAO
+from dao.tipoquartodao import TipoQuartoDAO
+
 from models.usuario import Usuario
 from models.hospede import Hospede
-from dao.hospededao import HospedeDAO
+from models.tipoquarto import TipoQuarto
+
 
 class View:
-    # Itens 
+    # Usuário
     @staticmethod
     def usuario_inserir(nome, fone, email, senha, tipoperfil, idperfil):
-        usuario = Usuario(0, nome, fone, email, senha, tipoperfil, idperfil)
-        UsuarioDAO.inserir(usuario)
+        u = Usuario(0, nome, fone, email, senha, tipoperfil, idperfil)
+        UsuarioDAO.inserir(u)
 
     @staticmethod
     def usuario_listar():
-        r = UsuarioDAO.listar()
-        # r.sort(key = lambda obj : obj.get_nome())
-        return r
+        u = UsuarioDAO.listar()
+        u.sort(key=lambda obj: obj.get_nome())
+        return u
 
     @staticmethod
     def usuario_listar_id(id):
@@ -22,28 +28,30 @@ class View:
 
     @staticmethod
     def usuario_atualizar(id, nome, fone, email, tipoperfil, idperfil):
-        usuario = Usuario(id, nome, fone, email, "********", tipoperfil, idperfil)
-        UsuarioDAO.atualizar(usuario)
-    
+        u = Usuario(id, nome, fone, email, "********", tipoperfil, idperfil)
+        UsuarioDAO.atualizar(u)
+
     @staticmethod
     def usuario_atualizar_senha(id, senha):
-        usuario = Usuario(id, "a", "a", "a", senha, "a", 0)
-        UsuarioDAO.atualizar_senha(usuario)
+        u = Usuario(id, "a", "a", "a", senha, "a", 0)
+        UsuarioDAO.atualizar_senha(u)
 
     @staticmethod
     def usuario_excluir(id: int):
-        usuario = Usuario(id, "a", "a", "a", "a", "a", 0)
-        UsuarioDAO.excluir(usuario)
-    
-    # Hospede
+        u = Usuario(id, "a", "a", "a", "a", "a", 0)
+        UsuarioDAO.excluir(u)
+
+    # Hóspede
     @staticmethod
     def hospede_inserir(id_usuario, endereco):
-        hospede = Hospede(0, id_usuario, endereco)
-        HospedeDAO.inserir(hospede)
+        h = Hospede(0, id_usuario, endereco)
+        HospedeDAO.inserir(h)
 
     @staticmethod
     def hospede_listar():
-        return HospedeDAO.listar()
+        h = HospedeDAO.listar()
+        h.sort(key=lambda obj: obj.get_endereco())
+        return h
 
     @staticmethod
     def hospede_listar_id(id):
@@ -51,10 +59,36 @@ class View:
 
     @staticmethod
     def hospede_atualizar(id_hospede, id_usuario, endereco):
-        hospede = Hospede(id_hospede, id_usuario, endereco)
-        HospedeDAO.atualizar(hospede)
+        h = Hospede(id_hospede, id_usuario, endereco)
+        HospedeDAO.atualizar(h)
 
     @staticmethod
     def hospede_excluir(id_hospede):
-        hospede = Hospede(id_hospede, 0, "")
-        HospedeDAO.excluir(hospede)
+        h = Hospede(id_hospede, 0, "")
+        HospedeDAO.excluir(h)
+
+    # TipoQuarto
+    @staticmethod
+    def tipoquarto_inserir(nome, descricao, capacidade, valor_diaria):
+        tq = TipoQuarto(0, nome, descricao, capacidade, valor_diaria)
+        TipoQuartoDAO.inserir(tq)
+
+    @staticmethod
+    def tipoquarto_listar():
+        tq = TipoQuartoDAO.listar()
+        tq.sort(key=lambda obj: obj.get_nome())
+        return tq
+
+    @staticmethod
+    def tipoquarto_listar_id(id):
+        return TipoQuartoDAO.listar_id(id)
+
+    @staticmethod
+    def tipoquarto_atualizar(id_tipoquarto, nome, descricao, capacidade, valor_diaria):
+        tq = TipoQuarto(id_tipoquarto, nome, descricao, capacidade, valor_diaria)
+        TipoQuartoDAO.atualizar(tq)
+
+    @staticmethod
+    def tipoquarto_excluir(id_tipoquarto):
+        tq = TipoQuarto(id_tipoquarto, "a", "a", 1, Decimal(0.01))
+        TipoQuartoDAO.excluir(tq)
