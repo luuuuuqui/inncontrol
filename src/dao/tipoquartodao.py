8ff37aad1f87173b4e44df93f6a8c60ce12bce6e
Lucas Duarte <196474158+luuuuuqui@users.noreply.github.com>
2026-01-06 12:51:43 -0300

    feat: adição de tipoquartodao.py

---
+++ src/dao/tipoquartodao.py
@@ -0,0 +1,85 @@
+from dao.dao import DAO
+from models.tipoquarto import TipoQuarto
+
+"""
+TipoQuarto(
+    id_tipo: int,
+    nome: str,
+    descricao: str,
+    capacidade: int,
+    valor_diaria: decimal,
+)
+"""
+"""
+TipoQuarto(
+    
+)
+"""
+
+
+class TipoQuartoDAO(DAO):
+    @classmethod
+    def inserir(cls, obj):
+        cls.abrir()
+        sql = """
+            INSERT INTO tipoquarto (nome, descricao, capacidade, valor_diaria)
+            VALUES (?, ?, ?, ?)
+        """
+        cls.execute(
+            sql,
+            (
+                obj.get_nome(),
+                obj.get_descricao(),
+                obj.get_capacidade(),
+                obj.get_valor_diaria()
+            ),
+        )
+        cls.fechar()
+
+    @classmethod
+    def listar(cls):
+        cls.abrir()
+        sql = "SELECT * FROM tipoquarto"
+        cursor = cls.execute(sql)
+        rows = cursor.fetchall()
+        objs = [
+            TipoQuarto(idtipoquarto, nome, descricao, capacidade, valor_diaria)
+            for (idtipoquarto, nome, descricao, capacidade, valor_diaria) in rows
+        ]
+
+        return objs
+
+    @classmethod
+    def listar_id(cls, id):
+        cls.abrir()
+        sql = "SELECT * FROM tipoquarto WHERE id = ?"
+        cursor = cls.execute(sql, (id,))
+        row = cursor.fetchone()
+        obj = TipoQuarto(*row) if row else None
+        cls.fechar()
+        return obj
+
+    @classmethod
+    def atualizar(cls, obj):
+        cls.abrir()
+        sql = """
+            UPDATE tipoquarto
+            SET nome=?, descricao=?, capacidade=?, valor_diaria=?
+            WHERE id=?
+        """
+        cls.execute(
+            sql, (
+                obj.get_nome(),
+                obj.get_descricao(),
+                obj.get_capacidade(),
+                obj.get_valor_diaria(),
+                obj.get_id_tipoquarto(),
+            ),
+        )
+
+    @classmethod
+    def excluir(cls, obj):
+        cls.abrir()
+        sql = "DELETE FROM tipoquarto WHERE id=?"
+        cls.execute(sql, (obj.get_id_tipoquarto(),))
+        cls.fechar()
