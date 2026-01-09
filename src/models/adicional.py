80eb81c1df79dbb6c1526cf829678c2cbe9d21f1
Lucas Duarte <196474158+luuuuuqui@users.noreply.github.com>
2026-01-09 13:30:52 -0300

    refactor(adicional): padroniza classe e passa valor para decimal

--- src/models/adicional.py
+++ src/models/adicional.py
@@ -1,36 +1,43 @@
-from decimal import Decimal as decimal
+from decimal import Decimal, ROUND_HALF_UP
 
 
 class Adicional:
-    def __init__(self, id_adicional: int, descricao: str, valor: decimal) -> None:
+    def __init__(
+        self, id_adicional: int, descricao: str, valor: Decimal | str | float
+    ) -> None:
         self.set_id_adicional(id_adicional)
         self.set_descricao(descricao)
         self.set_valor(valor)
 
     # Setters:
     def set_id_adicional(self, id_adicional: int):
-        if id_adicional <= 0:
+        if id_adicional < 0:
             raise ValueError("ID do adicional deve ser um inteiro positivo.")
-        self._id_adicional = id_adicional
+        self.__id_adicional = id_adicional
 
     def set_descricao(self, descricao: str) -> None:
-        if descricao == "":
+        if not descricao.strip():  # Forma mais pythonica de checar string vazia
             raise ValueError("Descrição do adicional não pode ser vazia.")
-        self._descricao = descricao
+        self.__descricao = descricao
 
-    def set_valor(self, valor: decimal) -> None:
+    def set_valor(self, valor: Decimal | str | float) -> None:
+        if not isinstance(valor, Decimal):
+            # Converter float para string antes de Decimal evita imprecisão flutuante
+            valor = Decimal(str(valor))
+
         if valor < 0:
             raise ValueError("Valor do adicional não pode ser negativo.")
-        self._valor = valor
+
+        self.__valor = valor.quantize(Decimal("0.00"), rounding=ROUND_HALF_UP)
 
     # Getters:
     def get_id_adicional(self) -> int:
-        return self._id_adicional
+        return self.__id_adicional
 
     def get_descricao(self) -> str:
         return self._descricao
 
-    def get_valor(self) -> decimal:
+    def get_valor(self) -> Decimal:
         return self._valor
 
     # Métodos:
@@ -40,3 +47,8 @@ class Adicional:
             "descricao": self.get_descricao(),
             "valor": self.get_valor(),
         }
+
+    def __str__(self) -> str:
+        return (
+            f"{self.get_id_adicional()} - {self.get_descricao()} - {self.get_valor():.2f}"
+        )
