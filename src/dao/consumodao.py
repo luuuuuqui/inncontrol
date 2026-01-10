bacb536d6a85d43a773ed2f4e188b86a2151d4b1
Lucas Duarte <196474158+luuuuuqui@users.noreply.github.com>
2026-01-10 15:57:54 -0300

    feat(dao): adiciona consumodao.py

---
+++ src/dao/consumodao.py
@@ -0,0 +1,56 @@
+from dao.dao import DAO
+from models.consumo import Consumo
+
+
+class ConsumoDAO(DAO):
+    @classmethod
+    def inserir(cls, obj):
+        cls.abrir()
+        sql = """
+            INSERT INTO consumo (id_reserva, id_adicional, quantidade, data_consumo)
+            VALUES (?, ?, ?, ?)
+        """
+        cls.execute(sql, (obj.get_id_reserva(), obj.get_id_adicional(), obj.get_quantidade(), obj.get_data_consumo()))
+        cls.fechar()
+
+    @classmethod
+    def listar(cls):
+        cls.abrir()
+        sql = "SELECT * FROM consumo"
+        cursor = cls.execute(sql)
+        rows = cursor.fetchall()
+        objs = [
+            Consumo(id_consumo, id_reserva, id_adicional, quantidade, data_consumo)
+            for (id_consumo, id_reserva, id_adicional, quantidade, data_consumo) in rows
+        ]
+
+        return objs
+
+    @classmethod
+    def listar_id(cls, id):
+        cls.abrir()
+        sql = "SELECT * FROM consumo WHERE id = ?"
+        cursor = cls.execute(sql, (id,))
+        row = cursor.fetchone()
+        obj = Consumo(*row) if row else None
+        cls.fechar()
+        return obj
+
+    @classmethod
+    def atualizar(cls, obj):
+        cls.abrir()
+        sql = """
+            UPDATE consumo
+            SET id_reserva=?, id_adicional=?, quantidade=?, data_consumo=?
+            WHERE id=?
+        """
+        cls.execute(
+            sql, (obj.get_id_reserva(), obj.get_id_adicional(), obj.get_quantidade(), obj.get_data_consumo(), obj.get_id_consumo())
+        )
+
+    @classmethod
+    def excluir(cls, obj):
+        cls.abrir()
+        sql = "DELETE FROM consumo WHERE id=?"
+        cls.execute(sql, (obj.get_id_consumo(),))
+        cls.fechar()
